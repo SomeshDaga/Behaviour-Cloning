79252c9a02171644f8fbb069032353be90ffd6f6
Somesh Daga <somesh.daga@alumni.ubc.ca>
2017-07-23 22:05:12 -0700

    Cleaning up script

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import numpy as np
+import cv2
 
 from keras.models import Sequential
 from keras.layers import Cropping2D
@@ -23,10 +24,7 @@ with open('../data/driving_log.csv') as csvfile:
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-import cv2
-import numpy as np
-import sklearn
-
+# Set scaling to shrink input image
 scaling = 0.3
 height = int(scaling * 160)
 width = int(scaling * 320)
@@ -34,6 +32,7 @@ width = int(scaling * 320)
 crop_top = int(50 * scaling)
 crop_bottom  = int(20 * scaling)
 
+# Generator to yield images as needed
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -45,18 +44,23 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 path = '../data/'
+
+                # Read in the images
                 center_image = cv2.imread(path + batch_sample[0])
                 left_image = cv2.imread(path + batch_sample[1].lstrip())
                 right_image = cv2.imread(path + batch_sample[2].lstrip())
 
+                # Shrink the images to the scaling factor specified
                 center_image = cv2.resize(center_image, (width, height), interpolation = cv2.INTER_AREA)
                 left_image = cv2.resize(left_image, (width, height), interpolation = cv2.INTER_AREA)
                 right_image = cv2.resize(right_image, (width, height), interpolation = cv2.INTER_AREA)
 
+                # Convert images to RGB color space
                 center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
                 left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
                 right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
 
+                # Initialize the angles
                 center_angle = float(batch_sample[3])
                 left_angle = center_angle + 0.25
                 right_angle = center_angle - 0.25
@@ -70,7 +74,7 @@ def generator(samples, batch_size=32):
                     left_angle = -1*left_angle
                     right_angle = -1*right_angle
 
-
+                # Append the images and angles to their respective arrays
                 images.append(center_image)
                 images.append(left_image)
                 images.append(right_image)
@@ -79,12 +83,11 @@ def generator(samples, batch_size=32):
                 angles.append(left_angle)
                 angles.append(right_angle)
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# compile and train the model using the generator function
+# Initialize the generators
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
@@ -92,8 +95,9 @@ model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Cropping2D(cropping=((crop_top, crop_bottom), (0,0)), input_shape=(height, width, 3)))
 model.add(Lambda(lambda x: x/255.0 - 0.5)) # Normalization
+
+# Define our LeNet architecture (model)
 model.add(Convolution2D(6,2,2,activation='tanh'))
-#model.add(Dropout(0.2))
 model.add(Convolution2D(6,2,2,activation='tanh'))
 model.add(Dropout(0.2))
 model.add(Flatten())
@@ -106,7 +110,7 @@ model.add(Dropout(0.2))
 model.add(Dense(1))
 model.add(Activation('tanh'))
 
-
+# Train use mean square errors as loss function, and the Adam optimizer
 model.compile(loss='mse', optimizer=Adam(lr=5e-4))
 model.fit_generator(train_generator, samples_per_epoch= 
             3*len(train_samples), validation_data=validation_generator, 
